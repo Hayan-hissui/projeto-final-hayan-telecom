e052eb31822bb803b498c4d29604ba41544542eb
Hayan-hissui <hayanpaes100@gmail.com>
2026-09-23 20:34:40 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/hayantelecom/ui/navigation/NavGraph.kt
@@ -0,0 +1,30 @@
+package com.example.hayantelecom.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun NavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavTarget.Home.route
+    ) {
+        composable(NavTarget.Home.route) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Hayan Telecom")
+            }
+        }
+    }
+}
